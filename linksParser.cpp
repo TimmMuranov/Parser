78cdba532a4b7a58d5240ca363036ac52d4d07bf
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-12-27 19:39:58 +0300

    Костяк сделан

--- linksParser.cpp
+++ linksParser.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+  string file;
+  string fileName;
+  cout << "Введите имя файла\n";
+  cin >> fileName;
+    ifstream in(fileName);
+    if (in.is_open()){
+      string line;
+      while (getline(in, line)){
+            file += line;
+        }
+    }
+  in.close();
+  cout << file << endl;
+}
