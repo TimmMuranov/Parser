5a84053a74f9995230fa0822c35e32eb821a7f97
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-12-27 19:53:37 +0300

    Update linksParser.cpp

--- linksParser.cpp
+++ linksParser.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
   string file;
   string fileName;
   cout << "Введите имя файла\n";
@@ -19,4 +18,18 @@ int main()
     }
   in.close();
   cout << file << endl;
+}
+
+string getLinks(string file){
+  string linkList = "";
+  int fileSize = file.length();
+  for(int x=0; x<fileSize; ++x){
+    if (file[x] == "\""){
+      int end = file.find("\"", x+1);
+      if(file.substr(x+1, 4) == "http"){
+        linkList += file.substr(x+1, end);
+      }
+    x += (end - (x+1)) + 1;
+    }
+  }
 }
