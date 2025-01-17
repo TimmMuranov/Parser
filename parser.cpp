e9bbe9e5be2705d5b57dd7e699b10d315675f578
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-01-17 16:49:43 +0000

    дебаг main

--- parser.cpp
+++ parser.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-int main(int argc = 0, char *argv[]){
-    if(argc == 0){
+int main(int argc, char *argv[]){
+    if(argc == 1){
         cout << "Аргументы не введены" << endl;
         return 1;
     }
-    cout << "Программа запущена";
+    cout << "Программа запущена" << endl;
+    cout << argc << endl;
+    cout << argv[1] << endl;
 }
